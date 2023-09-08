5fbe7ebccf24ed9d9160ce56b6d3676f09389a7f
Michael Zhu <michael.zhu@retool.com>
2023-09-07 20:57:16 -0700

    add feedback component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Feedback from "@/components/Feedback";
 import YoutubeLinkInput from "@/components/YoutubeLinkInput";
 
 export default function Home() {
   return (
-    <div className="w-full flex justify-center">
-      <YoutubeLinkInput onSubmit={(link) => console.log(link)} />;
+    <div className="w-full flex flex-col items-center">
+      <YoutubeLinkInput onSubmit={(link) => console.log(link)} />
+      <Feedback />
     </div>
   );
 }
