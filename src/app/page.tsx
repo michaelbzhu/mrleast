94524f2058d575dcc0e6663e44a6330328189a76
Michael Zhu <michael.zhu@retool.com>
2023-09-07 20:44:47 -0700

    basic styling and input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <input></input>;
+  return (
+    <div className="w-1/4 flex p-4">
+      <input
+        className="border border-black rounded-md w-full mr-2 text-sm p-1"
+        placeholder="youtube.com/watch?v=..."
+      ></input>
+      <button className="border-black border rounded-sm px-1 text-sm">
+        Submit
+      </button>
+    </div>
+  );
 }
